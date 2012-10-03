517b76f39b0db996ed97d4ce04cee6072a6e936c
Scott Caruso <scottcaruso@fullsail.edu>
2012-10-02 22:32:22 -0400

    Barebones functions to make camera and notification work.

--- AVF Demo App/js/main.js
+++ AVF Demo App/js/main.js
@@ -1,3 +1,14 @@
 //Scott Caruso
 //AVF 1210
 //Javascript for Landing App
+
+
+$("#camera").on("click", function(){
+	alert("This works!");
+	accessCamera();
+});
+
+$("#notification").on("click", function(){
+	alert("This works!");
+	makeBeep(3);
+});
