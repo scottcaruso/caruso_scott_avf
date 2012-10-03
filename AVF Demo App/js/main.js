ecb587baf7148df9a9ca5c61d00b5b498202bc7e
Scott Caruso <scottcaruso@fullsail.edu>
2012-10-03 13:09:37 -0400

    Geolocate, part one.

--- AVF Demo App/js/main.js
+++ AVF Demo App/js/main.js
@@ -12,7 +12,13 @@ $("#notification").on("click", function(){
 });
 
 $("#geolocation").on("click", function(){
-	accessGeolocation();
+	var latitude = prompt("Guessing game! Enter a number between -90 and 90 to try and guess your latitude within 5 degrees!","0");
+	if (latitude <=90 || latitude >=-90){
+		alert("You have entered a valid number!");
+		accessGeolocation();
+	} else {
+		alert("Please enter a valid number!");
+	}
 });
 
 $("#contacts").on("click", function(){
